ef7fd8d6761804afe998041ca2523f1cb765d3f5
Cezar Halmagean <cezar@mixandgo.com>
2022-08-19 09:30:20 +0300

    Added helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def new_user_form(**opts)
+    render(NewUserFormComponent.new(**opts))
+  end
+
+  def users(users)
+    render(UserDetailsComponent.with_collection(users))
+  end
+
+  def user_details(user)
+    render(UserDetailsComponent.new(user: user))
+  end
 end
