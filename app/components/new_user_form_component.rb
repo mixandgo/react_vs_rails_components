f24504c2086d919d1c863ee84f2043d6af5c7cd5
Cezar Halmagean <cezar@mixandgo.com>
2022-08-19 15:59:43 +0300

    Removed some dead code

--- app/components/new_user_form_component.rb
+++ app/components/new_user_form_component.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class NewUserFormComponent < ViewComponent::Base
-  def initialize(user_class: User, status: :initial)
+  def initialize(user_class: User)
     @user = user_class.new
-    @status = status
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
-  def new_user_form(**opts)
-    render(NewUserFormComponent.new(**opts))
+  def new_user_form
+    render NewUserFormComponent.new
   end
 
   def users(users)
-    render(UserDetailsComponent.with_collection(users))
+    render UserDetailsComponent.with_collection(users)
   end
 
   def user_details(user)
-    render(UserDetailsComponent.new(user: user))
+    render UserDetailsComponent.new(user: user)
   end
 end
